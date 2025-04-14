7205b47b5311fbc38d57621ce64749dc382ea5cf
Sibuchanda <sibuchanda457@gmail.com>
2025-04-14 18:54:28 +0530

    Checking whether git collaboration working or not

--- src/Pages/Helloworld.jsx
+++ src/Pages/Helloworld.jsx
@@ -8,6 +8,7 @@ const Helloworld = () => {
         <div>
             <div>
                 <h1 className='flex justify-center mt-2 text-4xl font-bold text-red-500 pt-[30px]'>-: Welcome to my profile :-</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ad nostrum dolorem delectus voluptatem velit ipsa ea aut eaque.</p>
             </div>
             <div className='flex flex-col  justify-center items-center h-[400px] mt-10'>
                 <div className='text-white text-2xl'>Hello world</div>
